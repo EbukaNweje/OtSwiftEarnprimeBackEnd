ae0a3476c7ce022783be9e2eefe7ce2838ddfe36
eflexTech <eflexmedia1@gmail.com>
2024-03-19 11:23:33 -0700

    G

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const register = require("../controllers/auth")
-const { check } = require('express-validator');
+const { check } = require('express-validator');const admin = require("../controllers/Admin");
+const myadmin = require("../controllers/Admin");
 
 const Routers = express.Router()
 
@@ -8,6 +9,8 @@ Routers.route("/register").post([
     check('email', 'Please include a valid email').isEmail(),
   ],register.register)
 Routers.route("/login").post(register.login)
+Routers.route("/adminregister").post(myadmin.register)
+Routers.route("/loginadmin").post(myadmin.login)
 Routers.route("/restLink/:id/:token").post(register.restLink).get(register.getrestlink)
 Routers.route("/verifyotp/:id").post(register.verifySuccessful)
 Routers.route("/resetotp/:id").post(register.resendotp)
